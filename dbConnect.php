73af23c7a40027494b5c28df8571497a144362ac
Kevin Truebe <kevin.truebe@gmail.com>
2017-12-13 13:00:31 -0800

    PHP Files
    php files to connect to the database

---
+++ dbConnect.php
@@ -0,0 +1,8 @@
+
+<?php
+$servername = "csusm.c0uo1rgt9ctn.us-west-2.rds.amazonaws.com";
+$uname = "cs441";
+$password = "csusmcs441";
+$dbname = "jetstreams_database";
+$con = mysqli_connect($servername, $uname, $password, $dbname) or die('Unable to Connect'); 
+ ?>
---
+++ getdatachicago.php
@@ -0,0 +1,15 @@
+
+<?php 
+$sql = "SELECT * FROM Chicago";
+require_once('dbConnect.php');
+//$con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+$r = mysqli_query($con,$sql);
+$result = array();
+while($row = mysqli_fetch_array($r)){
+ array_push($result,array(
+'Month'=>$row['Month'],
+'Price'=>$row['Price']
+    ));
+}
+echo json_encode(array('result'=>$result));
+mysqli_close($con);?>
